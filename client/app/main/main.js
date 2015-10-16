3300815dd97e947bf3c1dbda07101d30de0de6ad
Joep Wijnhoven <joep_wijnhoven@hotmail.com>
2015-10-16 10:33:56 +0200

    eerste versie

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('theFoolsApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
