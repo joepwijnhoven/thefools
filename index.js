2095fbf375ebda8581a2ab46ec167cc48b5775cc
Joep Wijnhoven <joep_wijnhoven@hotmail.com>
2015-10-18 11:42:27 +0200

    routing gefixed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 
-app.get('/', function(request, response) {
+app.get('*', function(request, response) {
   response.sendFile(path.join(__dirname+'/index.html'));
 });
 
