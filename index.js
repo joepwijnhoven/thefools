8ddab3d527129cdef284f7ae4e41d7eba521afbf
Joep Wijnhoven <joep_wijnhoven@hotmail.com>
2015-10-19 21:13:33 +0200

    index.js aangepast

--- index.js
+++ index.js
@@ -11,12 +11,8 @@ app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 
-app.get('*', function(request, response) {
-  response.sendFile(path.join(__dirname+'/index.html'));
+http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
+  console.log("? Express server listening at %s:%d ", app.get('ip'), app.get('port'));
+  server();
 });
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
 
